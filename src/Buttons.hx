6bf9bb00521cb644f2a8e5f18931a026959ab56f
7052roy <leinenga.r@hotmail.nl>
2015-04-28 18:07:30 +0200

    button

--- src/Buttons.hx
+++ src/Buttons.hx
@@ -1,5 +1,6 @@
 package ;
 import openfl.display.Sprite;
+import openfl.display.Bitmap;
 
 /**
  * ...
@@ -15,7 +16,9 @@ class Buttons extends Sprite
 	
 	public function startButton()
 	{
-		
+		var buttonGraphic:openfl.display.BitmapData = Assets.getBitmapData( "img/start.png" );
+		var bitmap:openfl.display.Bitmap = new Bitmap( buttonGraphic );
+		addChild( bitmap );
 	}
 	
 	public function endButton()
