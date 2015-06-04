47ef7595c8877d726ca3cec4176fac34f870237d
7052roy <leinenga.r@hotmail.nl>
2015-06-04 16:59:01 +0200

    finalizing

--- quizgame.sql
+++ quizgame.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 22 mei 2015 om 22:10
+-- Gegenereerd op: 04 jun 2015 om 15:25
 -- Serverversie: 5.6.24
 -- PHP-versie: 5.6.8
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `answer` (
   `Answer_ID` int(11) NOT NULL,
   `Question_ID` int(11) NOT NULL,
   `PossibleAnswer` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `answer`
@@ -54,7 +54,19 @@ INSERT INTO `answer` (`Answer_ID`, `Question_ID`, `PossibleAnswer`) VALUES
 (15, 5, 'Moreys Pier'),
 (16, 6, 'Top Thrill Dragster'),
 (17, 6, 'King Da Ka'),
-(18, 6, 'Stealth');
+(18, 6, 'Stealth'),
+(19, 7, 'Big Thunder Mountain'),
+(20, 7, 'Mission Space'),
+(21, 7, 'Space Mountain'),
+(22, 8, 'Vekoma'),
+(23, 8, 'B&M'),
+(24, 8, 'Premier Rides'),
+(25, 9, 'Six Flags America'),
+(26, 9, 'Six Flags Great America'),
+(27, 9, 'Six Flags Great Adventure'),
+(30, 10, 'Tornado'),
+(31, 10, 'Time Warp'),
+(32, 10, 'Volare');
 
 -- --------------------------------------------------------
 
@@ -66,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `highscores` (
   `HighScore_ID` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
   `Score` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `highscores`
@@ -74,7 +86,10 @@ CREATE TABLE IF NOT EXISTS `highscores` (
 
 INSERT INTO `highscores` (`HighScore_ID`, `Name`, `Score`) VALUES
 (1, 'roy', 0),
-(2, 'marco', 33767);
+(2, 'marco', 33767),
+(4, 'roy', 103400),
+(5, 'roy', 75559),
+(6, 'test', 86887);
 
 -- --------------------------------------------------------
 
@@ -87,7 +102,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `Question` varchar(100) NOT NULL,
   `Picture` varchar(50) NOT NULL,
   `CorrectAnswer` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `question`
@@ -99,7 +114,11 @@ INSERT INTO `question` (`Question_ID`, `Question`, `Picture`, `CorrectAnswer`) V
 (3, 'Where is this Roller coaster located?', 'Tornado', 'Avonturenpark Hellendoorn'),
 (4, 'Where is this Roller coaster located?', 'Boomerang', 'Walibi Belgium'),
 (5, 'Where is this Roller coaster located?', 'GreatWhite', 'Moreys Pier'),
-(6, 'What is the name of this coaster?', 'KingDaKa', 'King Da Ka');
+(6, 'What is the name of this coaster?', 'KingDaKa', 'King Da Ka'),
+(7, 'What is the name of the rollercoaster in the back?', 'SpaceMountain', 'Space Mountain'),
+(8, 'Which company build Joker''s Jinx?', 'Joker', 'Premier Rides'),
+(9, 'Where is this Roller coaster located?', 'RideOfSteel', 'Six Flags America'),
+(10, 'What is the model of this coaster called?', 'TimeWarp', 'Volare');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -131,17 +150,17 @@ ALTER TABLE `question`
 -- AUTO_INCREMENT voor een tabel `answer`
 --
 ALTER TABLE `answer`
-  MODIFY `Answer_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `Answer_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT voor een tabel `highscores`
 --
 ALTER TABLE `highscores`
-  MODIFY `HighScore_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `HighScore_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT voor een tabel `question`
 --
 ALTER TABLE `question`
-  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
